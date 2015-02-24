99443953728dcd4ecb20dd36d925696788e82cae
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2015-02-24 11:04:33 -0500

    Collision detection tests
    PointPolygonTraversal() is missing a part of the code. I need to see how
    I'll do that.

--- KillBox/physics.cpp
+++ KillBox/physics.cpp
@@ -16,6 +16,8 @@
 
 // What affects a thing's movement in space
 
+#include <cmath>
+
 #include "things.h"
 
 void Teleport(Player &Teleported, float NewPosX, float NewPosY, float NewPosZ, unsigned short NewAngle)
@@ -49,3 +51,92 @@ bool OnGround(Player &Object)
 	return true;
 }
 
+bool PointInPlayer(Player &Object, float PointX, float PointY, float PointZ)
+{
+	float d2Mesurment = pow(Object.PosX - PointX, 2) + pow(Object.PosY - PointY, 2);
+	//float Distance = sqrt(d2Mesurment + pow(Object.PosZ - PointZ, 2));
+
+	if (PointZ > Object.PosZ && Object.PosZ + Object.Height() < PointZ)
+	{
+		// Thee point is inside the player's Z coordinates
+
+		if (sqrt(d2Mesurment) <= Object.Radius());
+		{
+			// The point is inside the player's radius
+			return true;
+		}
+	}
+
+	// There was no collision
+	return false;
+}
+
+void HitScan(Player Players[], int NumberOfPlayers, float IpX, float IpY, float IpZ, float Hangle, float Vangle, int Damage)
+{
+	float Step = 1;		// Increment betweens tests
+	int Max = 2048;		// Max distance we will check in multiples of steps
+	bool Hit = false;	// Has the bullet already hit something?
+
+	for (int i = 0; i < Max && !Hit; i++)
+	{
+		// Until we reach the maximum distance to check...
+
+		// TODO: CHECK IF WE HIT A WALL
+
+		for (int p = 0; p < NumberOfPlayers && !Hit; p++)
+		{
+			// Check if the point is in the player
+			if (PointInPlayer(Players[p], IpX, IpY, IpZ))
+			{
+				// If the point is inside the player, then damage him. 
+				Players[p].HealthChange(Damage);
+				Hit = true;		// We hit something
+				break;
+			}
+		}
+
+		// Move the bullet forward
+		if (!Hit)
+		{
+			// NOTES: http://www.mathsisfun.com/sine-cosine-tangent.html
+			IpX += Step * cos(Hangle);
+			IpY += Step * sin(Hangle);
+			IpZ += Step * sin(Vangle);
+		}
+	}
+}
+
+bool PointPolygonTraversal(Level* Geometry, float PointX, float PointY, float PointZ, float Velocity, float Hangle, float Vangle)
+{
+	bool Hit = false;
+
+	// Test every wall
+	for (int w = 0; w < Geometry->ptrWalls->size(); w++)
+	{
+		// Go trough every vertex
+		for (int v = 0; v < Geometry->ptrWalls->at(w).ptrVertices->size(); v++)
+		{
+			// TODO: CHECK FOR COLISION INSIDE THE POLY
+		}
+	}
+
+	// If we hit geometry
+	return true;
+}
+
+bool PlayerCollision(Player &One, Player &Two)
+{
+	// The following takes in account that both players have the same height
+	if (abs(One.PosZ - Two.PosZ) <= One.Height)
+	{
+		// The players are at the same Z height, now test if they are in the same XY space 
+		if (pow(One.PosX - Two.PosX, 2) + pow(One.PosY - Two.PosY, 2) <= One.Radius() + Two.Radius());
+		{
+			// The players are inside each other
+			return true;
+		}
+	}
+
+	// There is no collision
+	return false;
+}
