18da5dfbd63c2393580138a2b44a891fa7a4f7d2
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2017-11-27 19:43:42 -0500

    Compilation scripts

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Set the working directory to the script's directory
+cd $(dirname "$0")
+cd KillBox
+
+# Compile the source files and execute
+g++ main.cpp command.cpp textread.cpp viewdraw.cpp things.cpp texture.cpp -std=c++11 -lglfw -lGL -lGLU -lglut -lSDL -o FPS_test && ./FPS_test $@
