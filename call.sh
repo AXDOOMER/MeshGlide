6e6022bb8860c02e66772f395cdfd6bdf84ca6c3
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2018-08-16 01:29:04 -0400

    Added a note on how to setup callgrind

--- call.sh
+++ call.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# How to setup this: https://baptiste-wicht.com/posts/2011/09/profile-c-application-with-callgrind-kcachegrind.html
+
 # Set the working directory to the script's directory
 cd $(dirname "$0")
 
