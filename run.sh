7e3c4b1c06d715aa4c62bc32072d82b417ea5a0f
Alexandre-Xavier Labonté-Lamoureux <alexandrexavier@live.ca>
2018-12-12 00:14:18 -0500

    run.sh: Add features 'cppcheck', 'info' and 'release'

--- run.sh
+++ run.sh
@@ -1,8 +1,31 @@
 #!/bin/bash
 
+# Name of the output file
+EXENAME=MeshGlide
+
 # Set the working directory to the script's directory
 cd $(dirname "$0")
 cd Project
 
-# Compile the source files and execute
-g++ *.cpp -std=c++11 -Wall -Wextra -lglfw -lGL -lGLU -lSDL2 -lSDL2_image -lzmq -o MeshGlide && ./MeshGlide $@
+# Compile the source code files and execute the program by default
+if [ "$#" -ge 1 ] && [ "$1" == "cppcheck" ]; then
+	cd ..
+	cppcheck --error-exitcode=1 -j2 -UTESTING -Iopl -Isrc -Isrc/Project Project/
+elif [ "$#" -ge 1 ] && [ "$1" == "info" ]; then
+	if [ -f $EXENAME ]; then
+		echo -n "File size in bytes is: "
+		du -b $EXENAME | cut -f 1
+		md5sum $EXENAME
+	else
+		echo "File not found!"
+	fi
+elif [ "$#" -ge 1 ] && [ "$1" == "release" ]; then
+	echo "Building release"
+	shift
+	echo "$EXENAME args: $@"
+	g++ *.cpp -std=c++11 -O2 -s -Wall -Wextra -lglfw -lGL -lGLU -lSDL2 -lSDL2_image -lzmq -o $EXENAME && ./$EXENAME $@
+else
+	echo "Building default"
+	echo "$EXENAME args: $@"
+	g++ *.cpp -std=c++11 -Wall -Wextra -lglfw -lGL -lGLU -lSDL2 -lSDL2_image -lzmq -o $EXENAME && ./$EXENAME $@
+fi
